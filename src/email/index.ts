d13a8734e6c12d4fa89be906a2e5be923a6ab2e3
Shreyansh-Sheth <shethshreyansh777@gmail.com>
2022-06-02 00:24:00 +0530

    added support to deploy and mail changes

--- src/email/index.ts
+++ src/email/index.ts
@@ -2,12 +2,12 @@ import { readFileSync } from "fs";
 import { logger } from "../utils";
 import ejs from "ejs";
 
-const verificationMailTemplate = readFileSync(
-  `${__dirname}\\templates\\magic-link.ejs`
-).toString();
+// const verificationMailTemplate = readFileSync(
+//   `${__}\\templates\\magic-link.ejs`
+// ).toString();
 
 export function sendVerificationMail(link: string, to: string) {
-  const template = ejs.compile(verificationMailTemplate, {});
+  const template = ejs.compile("", {});
   logger.log("info", template({ link }));
 }
 export function sendOtp(otp: string, to: string) {
